6cb8f18a4504f427ded25be68eb73bcad0893575
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-18 09:59:40 +0300

    navbar done plus general styles added

--- src/components/appNavbar/appNavbar.jsx
+++ src/components/appNavbar/appNavbar.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class AppNavbar extends Component {
   state = {};
   render() {
-    return <div className='main-nav'>Navbar</div>;
+    return (
+      <header className='main-nav'>
+        <a href='/'>
+          <strong>Keep</strong>Clone
+        </a>
+        <div className='search'>
+          <input type='search' placeholder='Search' />
+        </div>
+        <div>
+          <i className='fa fa-pencil'></i>
+        </div>
+      </header>
+    );
   }
 }
 
